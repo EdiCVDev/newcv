93047fd17ee42c693c868217f63b60202a50c4f6
EdiCVDev <johnmunera@confortvital.com.co>
2022-08-19 16:24:25 -0500

    dbheroku

--- db.php
+++ db.php
@@ -1,9 +1,16 @@
 <?php
-$conn = mysqli_connect(
+/* $conn = mysqli_connect(
     'localhost',
     'root',
     '',
     'bitnami_wordpress'
+); **/
+
+$conn = mysqli_connect(
+    'ec2-54-159-175-38.compute-1.amazonaws.com',
+    'rzpxwttsijfqjz',
+    'b89ca8fb71f23452c20b7acd6d632f8d835bfe4059233ba6a7addffae387d3fc',
+    'dbg19bjmev7jrl'
 );
 
 ?>
